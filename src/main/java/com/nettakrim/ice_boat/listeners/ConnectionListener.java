0063d907ebf92ceb7ec14a96e9af80d9154025eb
Nettakrim <you@example.com>
2023-05-07 12:25:07 +0100

    removed connection listener until i can make it work properly with multiverse

--- src/main/java/com/nettakrim/ice_boat/listeners/ConnectionListener.java
+++ src/main/java/com/nettakrim/ice_boat/listeners/ConnectionListener.java
@@ -13,27 +13,27 @@ import com.nettakrim.ice_boat.IceBoat.GameState;
 
 public class ConnectionListener implements Listener {
 
-    @EventHandler
-	public void onQuit(PlayerQuitEvent event) {
-		Player player = event.getPlayer();
-        if (IceBoat.instance.players.contains(player)) {
-            if (IceBoat.gameState == GameState.PLAYING) {
-                IceBoat.instance.killPlayer(player);
-            } else {
-                IceBoat.instance.waitingPlayerLeave(player);
-                if (player.isInsideVehicle()) player.getVehicle().remove();
-            }
-        }
-	}
+    //@EventHandler
+	//public void onQuit(PlayerQuitEvent event) {
+	//	Player player = event.getPlayer();
+    //    if (IceBoat.instance.players.contains(player)) {
+    //        if (IceBoat.gameState == GameState.PLAYING) {
+    //            IceBoat.instance.killPlayer(player);
+    //        } else {
+    //            IceBoat.instance.waitingPlayerLeave(player);
+    //            if (player.isInsideVehicle()) player.getVehicle().remove();
+    //        }
+    //    }
+	//}
 
-    @EventHandler
-	public void onJoin(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-        if (IceBoat.gameState == GameState.PLAYING) {
-            player.setGameMode(GameMode.SPECTATOR);
-        } else {
-            player.setGameMode(GameMode.ADVENTURE);
-            player.teleport(new Location(player.getWorld(), 0, IceBoat.config.getDouble("spawnHeight"), 0));
-        }
-	}
+    //@EventHandler
+	//public void onJoin(PlayerJoinEvent event) {
+	//	Player player = event.getPlayer();
+    //    if (IceBoat.gameState == GameState.PLAYING) {
+    //        player.setGameMode(GameMode.SPECTATOR);
+    //    } else {
+    //        player.setGameMode(GameMode.ADVENTURE);
+    //        player.teleport(new Location(player.getWorld(), 0, IceBoat.config.getDouble("spawnHeight"), 0));
+    //    }
+	//}
 }
